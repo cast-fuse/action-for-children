f5bd9c38e4b4a592c5cf2fb0f1df105ddfd25001
Andrew MacMurray <a.macmurray@icloud.com>
2017-06-06 12:54:38 +0100

    full coverage for user controller

--- test/action_for_children/web/controllers/user_controller_test.exs
+++ test/action_for_children/web/controllers/user_controller_test.exs
@@ -1,5 +1,6 @@
 defmodule ActionForChildren.Web.UserControllerTest do
   use ActionForChildren.Web.ConnCase
+  alias ActionForChildren.Accounts
 
   test "creates a new user", %{conn: conn} do
     conn = post conn, "/users"
@@ -7,5 +8,21 @@ defmodule ActionForChildren.Web.UserControllerTest do
 
     assert String.length(user.uuid) == 36
     assert redirected_to(conn) == user_path(conn, :show, user)
+  end
+
+  test "redirects to home if no user found", %{conn: conn} do
+    conn = get conn, "/users/123"
+
+    assert get_flash(conn, :error) =~ "sorry could not find that user"
+    assert redirected_to(conn) == page_path(conn, :index)
+  end
+
+  test "shows user with the correct shortcode and auto logs them in", %{conn: conn} do
+    {:ok, user} = Accounts.create_user(%{uuid: Ecto.UUID.generate()})
+    conn = get conn, user_path(conn, :show, user)
+    %{assigns: %{user: user}} = conn
+
+    assert String.length(user.uuid) == 36
+    assert html_response(conn, 200) =~ user.uuid
   end
 end
