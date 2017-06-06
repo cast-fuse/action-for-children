cd39d7261d555cbaaebac9109a8693ccbcd17154
Andrew MacMurray <a.macmurray@icloud.com>
2017-06-06 13:31:37 +0100

    add tests for page controller, modify auth_plug

--- test/action_for_children/web/controllers/page_controller_test.exs
+++ test/action_for_children/web/controllers/page_controller_test.exs
@@ -1,8 +1,45 @@
 defmodule ActionForChildren.Web.PageControllerTest do
   use ActionForChildren.Web.ConnCase
+  alias ActionForChildren.Accounts
 
-  test "GET /", %{conn: conn} do
+  test "non logged in user sees options to start conversation and enter code", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Action for Children"
+
+    contents = [
+      "Action for Children",
+      "intercom",
+      "Talk to an expert",
+      "form",
+      "user code",
+    ]
+
+    Enum.map contents, fn content ->
+      assert html_response(conn, 200) =~ content
+    end
+  end
+
+  test "logged in user only sees option to continue conversation", %{conn: conn} do
+    {:ok, user} = Accounts.create_user(%{uuid: Ecto.UUID.generate()})
+    conn = assign(conn, :user, user)
+    conn = get conn, "/"
+
+    contents = [
+      "Action for Children",
+      "intercom",
+      "Talk to an expert"
+    ]
+
+    excluded_content = [
+      "user code",
+      "put your code here"
+    ]
+
+    Enum.map excluded_content, fn content ->
+      refute html_response(conn, 200) =~ content
+    end
+
+    Enum.map contents, fn content ->
+      assert html_response(conn, 200) =~ content
+    end
   end
 end
