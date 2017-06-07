68879555f616bb61ae090d1f3f86f022b0994eae
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2017-06-07 11:01:09 +0100

    Make css helpers available in all views

--- lib/action_for_children/web/views/user_view.ex
+++ lib/action_for_children/web/views/user_view.ex
@@ -1,7 +1,6 @@
 defmodule ActionForChildren.Web.UserView do
   use ActionForChildren.Web, :view
   alias ActionForChildren.Accounts
-  import ActionForChildren.Web.CssHelpers
 
   def render_shortcode(user), do: Accounts.to_shortcode(user.uuid)
 end
