728e1c367edc3779119710e86330d66a04cdd12d
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2017-06-08 12:29:45 +0100

    Reduce repetition in callback show template

--- lib/action_for_children/web/views/callback_view.ex
+++ lib/action_for_children/web/views/callback_view.ex
@@ -2,4 +2,14 @@ defmodule ActionForChildren.Web.CallbackView do
   use ActionForChildren.Web, :view
 
   def day_class, do: "pa2 bg-light-gray w-20 gray tc"
+  def days, do: {["monday", "tuesday", "wednesday", "thursday"], ["friday", "saturday", "sunday", "any"]}
+  def times, do: ["morning", "afternoon", "evening"]
+  def day_conversions, do: %{"monday" => "mon",
+                             "tuesday" => "tue",
+                             "wednesday" => "wed",
+                             "thursday" => "thurs",
+                             "friday" => "fri",
+                             "saturday" => "sat",
+                             "sunday" => "sun",
+                             "any" => "any"}
 end
