05a4b330395643f008aee155930da5e7de10e524
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2017-06-09 17:30:14 +0100

    Write test for create action in callback controller

--- test/action_for_children/web/controllers/callback_controller_test.exs
+++ test/action_for_children/web/controllers/callback_controller_test.exs
@@ -26,4 +26,12 @@ defmodule ActionForChildren.Web.CallbackControllerTest do
     assert get_session(conn, :uuid) == nil
     assert redirected_to(conn) == page_path(conn, :index)
   end
+
+  @callback_info %{callback: %{topic: "Teething", phone: "071233345838", time: "morning", day: "monday"}}
+  test "can send message to intercom", %{conn: conn} do
+    user = insert_user(%{uuid: "EAC31107"})
+    conn = post conn, user_callback_path(conn, :create, user), @callback_info
+    assert get_flash(conn, :info) =~ "thanks we'll get in touch"
+    assert redirected_to(conn) == user_path(conn, :show, user)
+  end
 end
