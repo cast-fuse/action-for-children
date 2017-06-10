c68142554e5c5521fd92eb794d33e855b90fd2a0
Andrew MacMurray <a.macmurray@icloud.com>
2017-06-10 15:39:29 +0100

    embedded schema to render form errors

--- lib/action_for_children/web/controllers/callback_controller.ex
+++ lib/action_for_children/web/controllers/callback_controller.ex
@@ -1,25 +1,38 @@
 defmodule ActionForChildren.Web.CallbackController do
   use ActionForChildren.Web, :controller
   alias ActionForChildren.Web.Plugs.Auth
+  alias ActionForChildren.{Accounts, Callback}
   import ActionForChildren.Web.IntercomPrefferedTimes
 
   plug Auth
 
   def show(conn, _params) do
-    render conn, "show.html"
+    render conn, "show.html", changeset: Accounts.new_callback()
   end
 
-  def create(conn, %{"user_id" => user_id, "callback" => callback}) do
+  def create(conn, %{"callback" => callback} = params) do
+    case Accounts.validate_callback(%Callback{}, callback) do
+      {:ok, changeset} ->
+        conn
+        |> send_to_intercom(params, changeset)
+      {:error, changeset} ->
+        conn
+        |> put_flash(:error, "please correct the errors")
+        |> render("show.html", changeset: changeset)
+    end
+  end
+
+  defp send_to_intercom(conn, %{"user_id" => user_id, "callback" => callback}, changeset) do
     message = format_message(callback)
     case send_preferred_times(%{user_id: user_id, message: message}) do
       {:ok, :message_sent, _} ->
         conn
         |> put_flash(:info, "thanks we'll get in touch")
         |> redirect(to: user_path(conn, :show, user_id))
-      {:error, _reason} ->
+      {:error, reason} ->
         conn
-        |> put_flash(:error, "something went wrong")
-        |> render("show.html")
+        |> put_flash(:error, "error: #{reason}")
+        |> render("show.html", changeset: changeset)
     end
   end
 end
