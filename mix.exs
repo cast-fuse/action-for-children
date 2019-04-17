8c0ff822c70af764f36686fa5880cf79cd74a5cb
Ivan <ivangonzalez@rocketmail.com>
2019-04-17 11:18:46 -0500

    Add setup for new domain and update to phoenix 1.3.4

--- mix.exs
+++ mix.exs
@@ -2,51 +2,56 @@ defmodule ActionForChildren.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :action_for_children,
-     version: "0.0.1",
-     elixir: "~> 1.4",
-     elixirc_paths: elixirc_paths(Mix.env),
-     compilers: [:phoenix, :gettext] ++ Mix.compilers,
-     start_permanent: Mix.env == :prod,
-     test_coverage: [tool: ExCoveralls],
-     preferred_cli_env: ["coveralls": :test,
-                         "coveralls.detail": :test,
-                         "coveralls.post": :test,
-                         "coveralls.html": :test],
-     aliases: aliases(),
-     deps: deps()]
+    [
+      app: :action_for_children,
+      version: "0.0.1",
+      elixir: "~> 1.4",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      compilers: [:phoenix, :gettext] ++ Mix.compilers(),
+      start_permanent: Mix.env() == :prod,
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
+      aliases: aliases(),
+      deps: deps()
+    ]
   end
 
   # Configuration for the OTP application.
   #
   # Type `mix help compile.app` for more information.
   def application do
-    [mod: {ActionForChildren.Application, []},
-     extra_applications: [:logger, :runtime_tools]]
+    [mod: {ActionForChildren.Application, []}, extra_applications: [:logger, :runtime_tools]]
   end
 
   # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
-  defp elixirc_paths(_),     do: ["lib"]
+  defp elixirc_paths(_), do: ["lib"]
 
   # Specifies your project dependencies.
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.3.0"},
-     {:phoenix_pubsub, "~> 1.0"},
-     {:phoenix_ecto, "~> 3.2"},
-     {:postgrex, ">= 0.0.0"},
-     {:phoenix_html, "~> 2.10"},
-     {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"},
-     {:excoveralls, "~> 0.7.0"},
-     {:intercom, "~> 0.0.1"},
-     {:envy, "~> 1.1.1"},
-     {:sendgrid, "~> 1.8.0"},
-     {:mock, "~> 0.2.0", only: :test},
-     {:httpoison, "~> 1.0", override: true}]
+    [
+      {:phoenix, "~> 1.3.4"},
+      {:phoenix_pubsub, "~> 1.0"},
+      {:phoenix_ecto, "~> 3.2"},
+      {:postgrex, ">= 0.0.0"},
+      {:phoenix_html, "~> 2.10"},
+      {:phoenix_live_reload, "~> 1.0", only: :dev},
+      {:gettext, "~> 0.11"},
+      {:cowboy, "~> 1.0"},
+      {:excoveralls, "~> 0.7.0"},
+      {:intercom, "~> 0.0.1"},
+      {:envy, "~> 1.1.1"},
+      {:sendgrid, "~> 1.8.0"},
+      {:mock, "~> 0.2.0", only: :test},
+      {:httpoison, "~> 1.0", override: true}
+    ]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
@@ -56,8 +61,10 @@ defmodule ActionForChildren.Mixfile do
   #
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
-    ["ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
-     "ecto.reset": ["ecto.drop", "ecto.setup"],
-     "test": ["ecto.create --quiet", "ecto.migrate", "test"]]
+    [
+      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      "ecto.reset": ["ecto.drop", "ecto.setup"],
+      test: ["ecto.create --quiet", "ecto.migrate", "test"]
+    ]
   end
 end
