8117644dbfb399aef7e6bfa0b2665c5cd31d827a
lucymk <lucy.m.king92@gmail.com>
2020-05-15 16:21:34 +0100

    new email link

--- lib/action_for_children/web/controllers/user_controller.ex
+++ lib/action_for_children/web/controllers/user_controller.ex
@@ -36,21 +36,26 @@ defmodule ActionForChildrenWeb.UserController do
         {:ok, %User{} = updatedUser} =
           Accounts.update_token(user, %{token: new_token, token_expires: token_expires})
 
+        verification_link_scheme = if Mix.env() == :prod, do: "https://", else: "http://"
+        verification_link_host = System.get_env("HOST")
+        verification_link_token = updatedUser.token
+
+        verification_link =
+          "#{verification_link_scheme}#{verification_link_host}/token-verification?token=#{
+            verification_link_token
+          }"
+
         Email.build()
         |> Email.add_to(user.email)
         |> Email.put_from("parents@actionforchildren.org.uk")
         |> Email.put_subject("Action for Children email verification")
-        |> Email.put_html(
-          "<p>
-            <a href='#{ActionForChildrenWeb.Endpoint.url()}/token-verification?token=#{
-            updatedUser.token
-          }'>Click here</a> to verify your email.
+        |> Email.put_html("<p>
+            <a href='#{verification_link}'>Click here</a> to verify your email.
           </p>
           <p>
           You’ll be taken back to our live chat, where you can start a new conversation with a parenting coach, or see the previous conversations you’ve had.
           </p>
-          <p> Please note this link is only valid for one hour. </p>"
-        )
+          <p> Please note this link is only valid for one hour. </p>")
         |> Mailer.send()
 
         conn
